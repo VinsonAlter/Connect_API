8c62ee0b3b5d8849010ff43abd812a6585838ef9
Vinson <57568741+VinsonAlter@users.noreply.github.com>
2022-10-07 16:17:20 +0700

    Add files via upload

---
+++ test_response/index.js
@@ -0,0 +1,11 @@
+console.log('Hello World');
+// fetch('https://qa-interface.mitsubishi-motors.co.id/qa-api/DMSData/workorder/Read')
+//     .then(res => {
+//         if (res.ok) {
+//             console.log('SUCCESS')
+//         } else {
+//             console.log('NOT SUCCESS')
+//         }
+//     })
+//     .then(data => console.log(data))
+//     .catch(error => console.log('ERROR'))
